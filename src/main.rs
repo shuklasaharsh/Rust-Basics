7249926f9b6696c0273dc53befc5b2135c7c52c7
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2023-04-16 10:26:17 +0530

    chore: break fn

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-use std::io;
-use rand::{thread_rng, Rng};
-use std::cmp::Ordering;
+mod guess;
 
+use guess::guess_game;
 
 fn main() {
-    let mut rng = thread_rng();
-    println!("guess the number!");
-
-    // We ask the user to input the number
-    println!("Enter the number: ");
-
-    // Create a variable
-    let mut guess = String::new();
-
-    // Input the variable
-    io::stdin().read_line(&mut guess).expect("Cannot read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-
-
-    let secret_number: u32 = rng.gen_range(1..10);
-
-    // Now lets compare the guess with the secret number
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You win! :)"),
-    }
+    guess_game();
 }
