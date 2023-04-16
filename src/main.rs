db681b96f53f0313eccfef6e13be0d1c77a9cc1a
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2023-04-16 11:46:10 +0530

    feat? remove: all

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-mod guess;
-mod data_types;
-
-use std::thread::sleep;
-use std::time::Duration;
-
-
 fn main() {
-    let dur = Duration::new(1, 0);
-    let mut count: u8 = 10;
-    while count != 0 {
-        println!("{count}");
-        count -= 1;
-        sleep(dur);
-    }
-    println!("LIFT OFF!")
+
 }
